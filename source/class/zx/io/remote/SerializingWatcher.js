4a7755ebc088ab07a159f24093b39ba0320a0b4d
Patryk Malinowski <pmalinowski@vmn.digital>
2024-04-12 17:38:25 +0100

    trap empty endpoints bug

--- source/class/zx/io/remote/SerializingWatcher.js
+++ source/class/zx/io/remote/SerializingWatcher.js
@@ -37,6 +37,9 @@ qx.Class.define("zx.io.remote.SerializingWatcher", {
      * @override
      */
     _watchProperty(info, propertyName) {
+      if (info.endpoints?.length === 0) {
+        debugger;
+      }
       let object = info.object;
       let upname = qx.lang.String.firstUp(propertyName);
       let io = this.getClassIos().getClassIo(object.constructor);
