13614ee0067b271e71d85410943a9381a8e928f1
Patryk Milinowski <pmilinowski@vmn.digital>
2024-09-20 11:45:53 +0100

    feature: debounced alert email

---
+++ source/class/zx/server/email/AlertEmail.js
@@ -0,0 +1,86 @@
+const os = require("os");
+
+/**
+ * This class is used to send alerts via email to the server administrator.
+ * It is useful for things like alerting the admin of any errors in the server that need attention.
+ *
+ * It debounces the emails such that an alert email is only sent every 5 minutes or less,
+ * hence an email may have more than one alert.
+ */
+qx.Class.define("zx.server.email.AlertEmail", {
+  type: "singleton",
+  extend: qx.core.Object,
+  construct() {
+    super();
+    const MINUTE = 60 * 1000;
+    const INTERVAL_MS = 5 * this.constructor.INTERVAL_MINUTES * MINUTE;
+    // const INTERVAL_MS = 2000;//for testing
+    this.__debounce = new zx.utils.Debounce(this.__sendEmail.bind(this), INTERVAL_MS).set({ repeatedTrigger: "ignore" });
+  },
+  members: {
+    /**
+     * @type {zx.utils.Debounce}
+     */
+    __debounce: null,
+    /**
+     * @type {string}
+     * The body of the email, accumulating the alerts util the email is sent
+     */
+    __body: "",
+
+    /**
+     * Adds an alert to send to the alerts email
+     * @param {string} title Title of the alert
+     * @param {string} text Full text or message of the alert
+     */
+    alert(title, text) {
+      let message = [
+        `================== Alert at ${new Date().toISOString()} ==================`,
+        `================== Title: ${title ?? "No title"} ==================`,
+        text, //
+        "\n"
+      ].join("\n");
+
+      this.__body += message;
+
+      this.__debounce.run();
+    },
+
+    /**
+     * Sends the emails with alerts
+     */
+    async __sendEmail() {
+      let client = zx.server.email.SMTPClient.getSmtpClientImpl();
+      const config = zx.server.Config.getInstance().getConfigData();
+      let message = zx.server.email.EmailJS.createNewMessage({
+        from: config.smtpServer.fromAddr,
+        to: config.alertsEmail,
+        subject: `ZX Server alert for ${config.websiteName}, machine: ${os.hostname()}`,
+        text: this.__body
+      });
+
+      try {
+        await client.sendAsync(message);
+      } catch (err) {
+        this.error("Error sending alert email: " + err.message + "\n" + err.stack);
+      }
+
+      this.__body = "";
+    },
+
+    /**
+     *
+     * @returns {Promise<void>} A promise which resolves when all the queued alerts have been sent
+     */
+    async join() {
+      return this.__debounce.join();
+    }
+  },
+
+  statics: {
+    /**
+     * Every how many minutes minimum to send an alert email
+     */
+    INTERVAL_MINUTES: 5
+  }
+});
