665a26e3494b9a73596bc6a23863f0b19e9b96ef
Patryk Milinowski <pmilinowski@vmn.digital>
2024-10-09 13:21:01 +0100

    emailRenderer: remove malicious swallowing of exception + improve logging

--- source/class/zx/server/email/EmailRenderer.js
+++ source/class/zx/server/email/EmailRenderer.js
@@ -5,7 +5,7 @@ qx.Class.define("zx.server.email.EmailRenderer", {
   statics: {
     /**
      * @param {string} url URL of the webpage with the email content
-     * @param {(msg: string) => void | null} log Function to log messages
+     * @param {((msg: string) => void) | null} log Function to log messages
      * @returns {Promise<zx.server.email.Message[]>} Array of messages to send
      */
     async run(url, log) {
@@ -13,66 +13,68 @@ qx.Class.define("zx.server.email.EmailRenderer", {
       if (log === undefined) {
         log = console.log;
       }
-      try {
-        let config = grasshopper.services.ServicesConfig.getInstance().getConfigData();
+      let config = grasshopper.services.ServicesConfig.getInstance().getConfigData();
 
-        let controller = new zx.server.puppeteer.PuppeteerController(zx.server.puppeteer.api.EmailServerApi).set({
-          username: config.authUser,
-          password: config.authTokens["grasshopper.automatedLogin"] || null
-        });
+      let controller = new zx.server.puppeteer.PuppeteerController(zx.server.puppeteer.api.EmailServerApi).set({
+        username: config.authUser,
+        password: config.authTokens["grasshopper.automatedLogin"] || null
+      });
 
-        await controller.initialise(url);
-        log("Initialised browser controller");
+      controller.addListener("consoleLog", evt => {
+        let data = evt.getData();
+        log("Message from puppeteer: " + JSON.stringify(data));
+      });
 
-        let puppeteer = controller.getPuppeteer();
-        await puppeteer.waitForReadySignal();
-        log("Received ready signal");
+      log("Initializing controller...");
+      await controller.initialise(url);
+      log("Initialised browser controller");
 
-        let api = controller.getApi();
-        api.addListener("sendEmail", async evt => {
-          const {
-            htmlBody,
-            textBody,
-            /**@type {EmailParameters}*/
-            parameters
-          } = evt.getData();
-          log("Email body to send: " + htmlBody);
-          log("Email parameters: " + JSON.stringify(parameters, null, 2));
+      let puppeteer = controller.getPuppeteer();
+      await puppeteer.waitForReadySignal();
+      log("Received ready signal");
 
-          if (parameters.attachments) {
-            const attachments = new qx.data.Array();
-            for (let attachment of parameters.attachments) {
-              if (attachment instanceof zx.server.email.Attachment) {
-                attachments.push(attachment);
-                continue;
-              }
-              attachments.push(
-                new zx.server.email.Attachment().set({
-                  name: attachment.name,
-                  path: attachment.path
-                })
-              );
+      let api = controller.getApi();
+      api.addListener("sendEmail", async evt => {
+        const {
+          htmlBody,
+          textBody,
+          /**@type {EmailParameters}*/
+          parameters
+        } = evt.getData();
+        log("Email body to send: " + htmlBody);
+        log("Email parameters: " + JSON.stringify(parameters, null, 2));
+
+        if (parameters.attachments) {
+          const attachments = new qx.data.Array();
+          for (let attachment of parameters.attachments) {
+            if (attachment instanceof zx.server.email.Attachment) {
+              attachments.push(attachment);
+              continue;
             }
-            parameters.attachments = attachments;
+            attachments.push(
+              new zx.server.email.Attachment().set({
+                name: attachment.name,
+                path: attachment.path
+              })
+            );
           }
+          parameters.attachments = attachments;
+        }
 
-          // ensure all `undefined` values are replaced with `null`
-          for (const key in parameters) {
-            parameters[key] ??= null;
-          }
+        // ensure all `undefined` values are replaced with `null`
+        for (const key in parameters) {
+          parameters[key] ??= null;
+        }
 
-          let message = await zx.server.email.Message.compose({ parameters, htmlBody, textBody });
-          messages.push(message);
-          api.next();
-        });
+        let message = await zx.server.email.Message.compose({ parameters, htmlBody, textBody });
+        messages.push(message);
+        api.next();
+      });
 
-        await api.start();
-        log("Started API");
-        await controller.promiseFinished();
-        log("Finished browser controller");
-      } catch (ex) {
-        console.error("Exception in client: " + (ex.stack || ex));
-      }
+      await api.start();
+      log("Started API");
+      await controller.promiseFinished();
+      log("Finished browser controller");
       return messages;
     }
   }
